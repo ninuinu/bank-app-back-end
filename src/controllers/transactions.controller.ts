19ec475ecb5e8075d2ee9426a02d840db96fd885
Helena Rosenzweig <helena.rosenzw@gmail.com>
2022-06-20 13:10:52 +0200

    create gettransactions skeleton

--- src/controllers/transactions.controller.ts
+++ src/controllers/transactions.controller.ts
@@ -1,15 +1,20 @@
-import {Request, Response} from "express";
+import { Request, Response } from "express";
 
 export function getTransactions(req: Request, res: Response) {
-    try {
-        console.log("hello");
-        const accountNumber = req.query.accountNumber as string;
-        return res.send(accountNumber);
-    } catch (error: any) {
-        return res.send(error);
-    }
+  try {
+    const accountNumber = req.query.accountNumber;
+    return res.send(accountNumber);
+  } catch (error: any) {
+    return res.send(error);
+  }
 }
 
 export function getTransaction(req: Request, res: Response) {
-    return null
+  try {
+    const transactionId = req.query.transactionId;
+    // fetch transaction
+    return null;
+  } catch (error: any) {
+    return res.send(error);
+  }
 }
