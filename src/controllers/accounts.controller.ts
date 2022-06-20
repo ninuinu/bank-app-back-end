e6b199eca1dc841b84ec901d50a05403d7080fe8
Helena Rosenzweig <helena.rosenzw@gmail.com>
2022-06-20 14:04:05 +0200

    add getaccounts function in resp controller and create users table in database

--- src/controllers/accounts.controller.ts
+++ src/controllers/accounts.controller.ts
@@ -1,34 +1,33 @@
-import {Request, Response} from "express";
-import {accountsTable} from "../database/accounts.table";
+import { Request, Response } from "express";
+import { accountsTable } from "../database/accounts.table";
 
 export function getAccounts(req: Request, res: Response) {
-    try {
-        const userId = req.query.ownerId;
-        const accounts = accountsTable.filter(
-            (user) => user.ownerId === Number(userId)
-        );
-        return res.send(accounts);
-    } catch (error: any) {
-        return res.send(error);
-    }
+  try {
+    const userId = req.query.userId;
+    const accounts = accountsTable.filter(
+      (account) => account.userId === Number(userId)
+    );
+    return res.send(accounts);
+  } catch (error: any) {
+    return res.send(error);
+  }
 }
 
-
 export function getAccount(req: Request, res: Response) {
-    return null
+  return null;
 }
 
 // bonus
 export function updateAccountName(req: Request, res: Response) {
-    return null
+  return null;
 }
 
 // bonus
 export function createAccount(req: Request, res: Response) {
-    return null
+  return null;
 }
 
 // bonus
 export function deleteAccount(req: Request, res: Response) {
-    return null
+  return null;
 }
---
+++ src/database/users.table.ts
@@ -0,0 +1,9 @@
+export const customersTable = [
+    {
+        customerId: 2451,
+        personalNumber: '620341-8732',
+        firstName: 'Ragnar',
+        lastName: 'Petterson',
+    }
+]
+
