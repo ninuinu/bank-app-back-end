8b776702fb8af699e8e13993d9cfa5c8a82c44c2
Helena Rosenzweig <helena.rosenzw@gmail.com>
2022-06-20 13:19:44 +0200

    create transactions database table

---
+++ src/database/transactions.table.ts
@@ -0,0 +1,39 @@
+export const transactionsTable = [
+    {
+        date: "2021-12-31",
+        account: 12322,
+        amount: 345,
+        counterparty: "Nordea",
+        id: 1
+    },
+    {
+        date: "2021-12-31",
+        account: 2022,
+        amount: -2424,
+        counterparty: "Swedbank",
+        id: 2
+    },
+    {
+        date: "2021-12-31",
+        account: 12322,
+        amount: 4532,
+        counterparty: "Nordea",
+        id: 3
+    },
+    {
+        date: "2021-12-31",
+        account: 2022,
+        amount: 6756,
+        counterparty: "Nordea",
+        id: 4
+    },
+    {
+        date: "2021-12-31",
+        account: 1337,
+        amount: 345,
+        counterparty: "Nordea",
+        id: 5
+
+    }
+]
+
