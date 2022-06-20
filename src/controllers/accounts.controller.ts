10caf5119180d369fc230bf0b985a52bb6048433
Helena Rosenzweig <helena.rosenzw@gmail.com>
2022-06-20 12:43:30 +0200

    create controller files

---
+++ src/controllers/accounts.controller.ts
@@ -0,0 +1,9 @@
+import {Request, Response} from "express";
+
+export function getAccounts(req:Request, res:Response){
+    return null
+}
+
+export function getAccount(req:Request, res:Response){
+    return null
+}
---
+++ src/controllers/transactions.controller.ts
@@ -0,0 +1,8 @@
+import {Request, Response} from "express";
+
+export function getTransactions(req:Request, res:Response){
+    return null
+}
+export function getTransaction(req:Request, res:Response){
+    return null
+}
